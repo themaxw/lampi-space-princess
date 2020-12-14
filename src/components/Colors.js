c2b0992f28d735c2b42766bcf7d327ed4073c072
Max Weidauer <weidauer@campus.tu-berlin.de>
2020-12-14 03:05:34 +0100

    aye it do work kinda

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import Rect from './Rect'
+import NewRect from './NewRect'
 
 class Colors extends Component {
     constructor(props) {
@@ -21,19 +22,40 @@ class Colors extends Component {
         })
     };
 
-    handleChangedColors(key, newColor) {
-        console.log(this)
-        var i = this.state.colors.findIndex(key);
-        this.state.colors[i] = newColor
-        console.log(this.state.colors);
+    handleChangedColors = (index, newColor) => {
+        const newColors = this.state.colors.slice();
+        newColors[index] = newColor;
+        this.setState({ colors: newColors });
     };
 
+    addNewColor = (color) => {
+        this.setState({ colors: [...this.state.colors, color] });
+        this.sendPalette();
+    };
+
+    removeColor = (index) => {
+        this.setState({ colors: this.state.colors.filter((_, i) => i !== index) })
+        this.sendPalette();
+    };
+
+    sendPalette = () => {
+        axios.put(this.props.url + "/colors", this.state.colors);
+    };
     render() {
         return (<div>
-            {this.state.colors.map(color => {
-                return (<Rect submitColorPalette={this.handleChangedColors} color={color} key={color} />)
+            {this.state.colors.map((color, index) => {
+                return (<Rect
+                    submitColorPalette={this.sendPalette}
+                    changeColors={this.handleChangedColors}
+                    removeColor={this.removeColor}
+                    color={color}
+                    key={index}
+                    index={index}
+                />)
             })}
-            <div onClick={() => console.log(this.state.colors)}>log colors</div>
+            <NewRect
+                addNewColor={this.addNewColor}
+            />
         </div>);
     }
 }
--- src/components/NewRect.js
+++ src/components/NewRect.js
@@ -1,41 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { ChromePicker } from "react-color";
-class Rect extends Component {
+import addIcon from './add.svg'
+
+class NewRect extends Component {
     static propTypes = {
         width: PropTypes.number,
         height: PropTypes.number,
         color: PropTypes.string,
-        submitColorPalette: PropTypes.func,
-        changeColors: PropTypes.func,
-        removeColor: PropTypes.func,
+        addNewColor: PropTypes.func,
+
         index: PropTypes.number
     }
-
     static defaultProps = {
         width: "30px",
         height: "30px",
+        color: "#1b1f22"
     }
-
     state = {
         color: this.props.color,
         displayColorPicker: false
     }
-
     toggleColorPicker = () => {
         this.setState({ displayColorPicker: !this.state.displayColorPicker })
     }
     handleClose = () => {
         this.setState({ displayColorPicker: false });
-        if (this.state.color === "#000000") {
-            this.props.removeColor(this.props.index);
-        } else {
-            this.props.submitColorPalette();
-        }
+        this.props.addNewColor(this.state.color);
+
+
     };
     handleChange = (color) => {
         this.setState({ color: color.hex });
-        this.props.changeColors(this.props.index, color.hex);
     }
     render() {
         const popover = {
@@ -49,19 +45,21 @@ class Rect extends Component {
             bottom: '0px',
             left: '0px',
         }
-        console.log(this.props.width)
         return (
             <div style={{ display: "inline-block" }}>
                 <div
-                    style={{ width: this.props.width, height: this.props.height, backgroundColor: this.state.color, display: "inline-block" }}
+                    style={{ width: this.props.width, height: this.props.height, backgroundColor: this.props.color, display: "inline-block" }}
                     onClick={this.toggleColorPicker}
-                />
-                { this.state.displayColorPicker ? <div style={popover}>
-                    <div style={cover} onClick={this.handleClose} />
-                    <ChromePicker color={this.state.color} onChangeComplete={this.handleChange} disableAlpha={true} />
-                </div> : null}
+                >
+                    <img src={addIcon} alt="add new color" />
+                </div>
+                { this.state.displayColorPicker ?
+                    <div style={popover}>
+                        <div style={cover} onClick={this.handleClose} />
+                        <ChromePicker color={this.state.color} onChangeComplete={this.handleChange} disableAlpha={true} />
+                    </div> : null}
             </div>);
     }
 }
 
-export default Rect;
+export default NewRect;
